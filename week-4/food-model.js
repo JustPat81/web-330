6e0f9871ab63ce043c5fdcd6e62625c30ba291d8
Patrick Wolff <pat.wolff81@gmail.com>
2022-06-19 17:43:00 -0500

    Adding week 4 assignments

---
+++ week-4/food-model.js
@@ -0,0 +1,16 @@
+/*
+* Title: food-model.js
+* Author: Patrick Wolff
+* Date: 19 June 2022
+* Description: Creating class FoodModel with constructors.
+*/
+
+export class FoodModel {
+
+//Creating constructor with fields
+constructor(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+    }
+}
